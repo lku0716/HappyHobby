d4aa2ae265f11fcae1ad9a6f73f4968aaa68302a
kyungup <dlrkql0007@gmail.com>
2022-06-02 01:22:45 +0900

    Happy Hobby App

---
+++ app/src/main/java/com/sungkyul/happyhobby/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sungkyul.happyhobby;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
