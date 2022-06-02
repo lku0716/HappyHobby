0bf82af25250217ae72be83f6ad5492c6af58298
kyungup <dlrkql0007@gmail.com>
2022-06-02 23:36:04 +0900

    Happy Hobby App

--- app/src/main/java/com/sungkyul/happyhobby/MainActivity.java
+++ app/src/main/java/com/sungkyul/happyhobby/MainActivity.java
@@ -1,9 +1,9 @@
 package com.sungkyul.happyhobby;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/sungkyul/happyhobby/SplashActivity.java
+++ app/src/main/java/com/sungkyul/happyhobby/SplashActivity.java
@@ -3,10 +3,9 @@ package com.sungkyul.happyhobby;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.support.v7.app.AppCompatActivity;
-
-public class SplashActivity extends AppCompatActivity{
+public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
